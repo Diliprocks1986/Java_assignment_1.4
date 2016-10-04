77f4554b3f8164dced8ba7072a319fc34070c29e
Dilip pashi <dilip.pashi143@gmail.com>
2016-10-04 15:30:44 +0530

    AcadGild

---
+++ ASCII.java
@@ -0,0 +1,10 @@
+class ASCII {
+	public static void main(String[] args){
+		//For Loop for Display 65 to 90 ASCII Value
+		for (int i = 65; i <=90; i++){
+			//Print All value while Loop is true
+			System.out.println(i + " ASCII Value is : " + (char)i); 
+			
+		}
+	}
+}
